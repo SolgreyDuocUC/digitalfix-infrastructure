37180502ed67a9abf2e40f35a6c58f0eab1df65d
VA- Alumno <VA-Alumno@duoc.acad>
2026-09-07 20:09:44 -0300

    first commit

---
+++ src/main/java/df/digitalfixinfrastructure/DigitalfixInfrastructureApplication.java
@@ -0,0 +1,14 @@
+package df.digitalfixinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalfixInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalfixInfrastructureApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/df/digitalfixinfrastructure/DigitalfixInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package df.digitalfixinfrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalfixInfrastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
